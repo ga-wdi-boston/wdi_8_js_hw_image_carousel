16da677bf011e3fb9d62b5104c7705e866ace905
Laure Fischer <laurefischer27@gmail.com>
2014-02-24 23:06:40 -0500

    changed app to display simple autoscrolling slideshow

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
-$(function(){
-  $('#second-pic').hide();
-  $('#third-pic').hide();
+$(function() {
 
-  $('#first-pic').click(function() {
-    $('#first-pic').slideUp({'direction': 'left', 'duration': 3000});
-    $('#second-pic').show({'direction': 'top', "duration": 3000});
-  });
+  var slider = $('#carousel'),
+    slide = $('li'),
+    transition_time = 1000,
+    time_between_slides = 2000,
+    interval;
 
-  $('#second-pic').click(function() {
-    $('#second-pic').slideUp({'direction': 'left', 'duration': 3000});
-    $('#third-pic').show({'direction': 'top', "duration": 3000});
-  });
+  function slides(){
+    return slider.find(slide);
+  };
 
-  $('#third-pic').click(function() {
-    $('#third-pic').slideUp({'direction': 'left', 'duration': 3000});
-    $('#first-pic').show({'direction': 'top', "duration": 3000});
-  });
+  slides().first().addClass('active');
+  slides().first().fadeIn(transition_time);
+
+  // auto scroll
+  interval = setInterval(
+    function(){
+      var i = slider.find('.active').index();
+
+      slides().eq(i).removeClass('active');
+      slides().eq(i).fadeOut(transition_time);
+
+      if(slides().length === i + 1) {
+        i =-1;
+      };
+
+      slides().eq(i + 1).fadeIn(transition_time);
+      slides().eq(i + 1).addClass('active');
+    }
+    , transition_time + time_between_slides
+  );
 });
 
+// $(function(){
+//   $('#second-pic').hide();
+//   $('#third-pic').hide();
+
+//   $('#first-pic').click(function() {
+//     $('#first-pic').slideUp({'direction': 'left', 'duration': 3000});
+//     $('#second-pic').show({'direction': 'top', "duration": 3000});
+//   });
+
+//   $('#second-pic').click(function() {
+//     $('#second-pic').slideUp({'direction': 'left', 'duration': 3000});
+//     $('#third-pic').show({'direction': 'top', "duration": 3000});
+//   });
+
+//   $('#third-pic').click(function() {
+//     $('#third-pic').slideUp({'direction': 'left', 'duration': 3000});
+//     $('#first-pic').show({'direction': 'top', "duration": 3000});
+//   });
+// });
