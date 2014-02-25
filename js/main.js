f42a74b9c0bef315db66624c7726d9e4d30d899d
Jack Henley <jack.d.henley@gmail.com>
2014-02-24 23:14:42 -0500

    Added next image button

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
+$(document).ready(function(){
+
+	$('#next-image').click(function() {
+		carouselApp.slide();
+	});
+
+});
+
 var carouselApp = {}
 
 carouselApp.slide = function() {
-	// var active,
-	// 	on_deck,
-	// 	in_the_hole;
 
 	var $active = $('#carousel img.active');
 	var $on_deck = $('#carousel img.on-deck');
 	var $in_the_hole = $('#carousel img.in-the-hole');
 
-	// active[0].classList.add('in-the-hole');
-	// active[0].classList.remove('active');
+
 	$active.addClass('in-the-hole');
 	$active.removeClass('active');
 
-	// on_deck[0].classList.add('active');
-	// on_deck[0].classList.remove('on-deck');
 	$on_deck.addClass('active');
 	$on_deck.removeClass('on-deck');
 
-	// in_the_hole[0].classList.add('on-deck');
-	// in_the_hole[0].classList.remove('in-the-hole');
 	$in_the_hole.addClass('on-deck');
 	$in_the_hole.removeClass('in-the-hole');
 };
 
-setInterval(carouselApp.slide, 3000)
+//setInterval(carouselApp.slide, 3000)
