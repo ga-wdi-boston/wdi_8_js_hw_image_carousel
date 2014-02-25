60869d1d2901ce921a232e2f3bc20039976682c9
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-24 21:36:07 -0500

    now with indicators for the images

--- js/main.js
+++ js/main.js
@@ -1,41 +1,51 @@
 
 var jknsCarousel = {};
-
-
 //when loaded
 $(document).ready(function(){
 	// get by class
 	jknsCarousel.CarouselElements = $('.item');
 
 	// reveal next image
-	jknsCarousel.play = function(){
+	jknsCarousel.playImage = function(){
 		var $curr = $('.item.active'),
 		    $first = $('.item').first(),
 		    $last = $('.item').last(),
 		    $next;
-
 		// Step 1: Make currently active item, inactive
 		$curr.removeClass('active');
-
 		// Step 2: make
 		$next = $curr.next();
 
 		if($next.length === 0) {
 			$next = $first;
 		}
-
 		// Step 3: make next item active
 		$next.addClass('active');
 	};
+	jknsCarousel.playIndicator = function(){
+		$('div[data-height]').text()
+		var $curr = $('.indicator.active'),
+		    $first = $('.indicator').first(),
+		    $last = $('.indicator').last(),
+		    $next;
+		// Step 1: Make currently active item, inactive
+		$curr.removeClass('active');
+		// Step 2: make
+		$next = $curr.next();
 
+		if($next.length === 0) {
+			$next = $first;
+		}
+		// Step 3: make next item active
+		$next.addClass('active');
+	};
 	//wait
-	setInterval(jknsCarousel.play, 3000);
+	setInterval(jknsCarousel.playImage, 3000);
+	setInterval(jknsCarousel.playIndicator, 3000);
+
+
 });
 
 
 
 
-
-// remove image from list
-// add it to the end of the list
-// then start the cycle again
