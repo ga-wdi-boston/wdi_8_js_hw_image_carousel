3864dd2b45f9ac0958a5e546ff9201b2eed45d08
emreiser <emilie.reiser@gmail.com>
2014-02-24 22:22:17 -0500

    Added logic to stop right and left scroll at end of list. Changed image to square, fixed margin on lis.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 $(document).ready(function(){
+
 	$('#left').click(CarouselApp.slideLeft);
 	$('#right').click(CarouselApp.slideRight);
+	// $("[data-slide-to='0']").click(CarouselApp.setTo("0px"));
+	// $("[data-slide-to='1']").click(CarouselApp.setTo("-500px"));
+	// $("[data-slide-to='2']").click(CarouselApp.setTo("-1005px"));
 });
 
 CarouselApp = {};
 
-CarouselApp.slideLeft = function(){
-	$('#image-list').animate({ "left": "-=500px" }, "slow" )
-}
+CarouselApp.slideLeft = function(event){
+	var list = $('#image-list');
+	var size = $('#image-list li').size();
+	if(list.position().left > ((size - 1) * -400)){
+		list.animate({ "left": "-=400px" }, 3000 )
+	};
+};
 
-CarouselApp.slideRight = function(){
-	$('#image-list').animate({ "left": "+=500px" }, "slow" )
-}
+CarouselApp.slideRight = function(event){
+	var list = $('#image-list');
+	var size = $('#image-list li').size();
+	if(list.position().left < 0) {
+		$('#image-list').animate({ "left": "+=400px" }, 3000 )
+	};
+};
+
+CarouselApp.setTo = function(position){
+	$('#image-list').animate({ "left": position }, 3000 );
+};
 
