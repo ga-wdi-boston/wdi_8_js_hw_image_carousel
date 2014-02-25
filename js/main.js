673a44938e1785b4fa6ffd25f0c4bdd84d4151a8
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-25 01:53:13 -0500

    carousel autoscrolls

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+
+var carousel = $('#carousel'),
+    picture = $('li'),
+    shift_time = 2000,
+    shift_distance = 4000,
+    interval;
+
+function pictures(){
+    return carousel.find(picture);
+};
+
+pictures().first().addClass('active');
+pictures().first().fadeIn(shift_time);
+
+
+interval = setInterval(
+    function(){
+    var i = carousel.find('.active').index();
+
+    pictures().eq(i).removeClass('active');
+    pictures().eq(i).fadeOut(shift_time);
+    if(pictures().length === i + 1) {
+        i =-1;
+    };
+
+    pictures().eq(i + 1).fadeIn(shift_time);
+    pictures().eq(i + 1).addClass('active');
+    }
+     , shift_time + shift_distance
+    );
+});
+
