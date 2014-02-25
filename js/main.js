d055a08a4f177546b54637363d8c8ccc99473b32
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-24 21:18:03 -0500

    now has the automatic functionality for a slide show

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 
-// when loaded
-// wait
-// move the list of images left by the image's width
+var jknsCarousel = {};
+
+
+//when loaded
+$(document).ready(function(){
+	// get by class
+	jknsCarousel.CarouselElements = $('.item');
+
+	// reveal next image
+	jknsCarousel.play = function(){
+		var $curr = $('.item.active'),
+		    $first = $('.item').first(),
+		    $last = $('.item').last(),
+		    $next;
+
+		// Step 1: Make currently active item, inactive
+		$curr.removeClass('active');
+
+		// Step 2: make
+		$next = $curr.next();
+
+		if($next.length === 0) {
+			$next = $first;
+		}
+
+		// Step 3: make next item active
+		$next.addClass('active');
+	};
+
+	//wait
+	setInterval(jknsCarousel.play, 3000);
+});
+
+
+
+
+
 // remove image from list
 // add it to the end of the list
 // then start the cycle again
